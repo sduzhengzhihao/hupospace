ec47b4831d33bd0a84445b08f0857252dcb3e384
zhengzhihao.hupo <zhengzhihao.hupo@bytedance.com>
2020-12-14 16:38:17 +0800

    OC重构

--- myblog/myblog/ViewController.m
+++ myblog/myblog/ViewController.m
@@ -7,23 +7,20 @@
 //
 
 #import "ViewController.h"
-#import "Masonry.h"
 #import "AFNetworking.h"
-#import "YYText.h"
+#import <Masonry.h>
 
 @interface ViewController ()<UIScrollViewDelegate,UITableViewDelegate,UITableViewDataSource>
-@property (nonatomic, strong) UILabel *label;
-@property (nonatomic, strong) UILabel *label2;
+@property (nonatomic, strong) UILabel *headline;
+@property (nonatomic, strong) UILabel *subhead;
 @property (nonatomic, strong) UIView *view1;
+@property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIScrollView *scrollViewbg;
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) UIPageControl *pageControl;
 @property(nonatomic,weak)NSTimer *timer;
 @property(nonatomic,strong)NSDictionary *bigDic;
-@property (nonatomic, strong) YYTextLayout *layout;
-@property (nonatomic, assign) CGFloat cellHeight;
-@property (nonatomic, assign) CGRect tableRect;
 @end
 
 @implementation ViewController
@@ -34,179 +31,189 @@
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     [manager GET:@"https://www.hupospace.com/wp-admin/includes/findtext.php" parameters:nil headers:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
         self.bigDic = responseObject;
-        NSLog(@"JSON: %@", self->_bigDic);
         [self.tableView reloadData];
-        } failure:^(NSURLSessionTask *operation, NSError *error) {
-            NSLog(@"Error: %@", error);
-        }];
-    //导航栏
-    
-    //顶部图片
-    UIImageView *imageViewbg=[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"背景"]];
-     imageViewbg.frame=CGRectMake(0,0,414, 270);
-     imageViewbg.contentMode=UIViewContentModeScaleToFill;
-     imageViewbg.clipsToBounds=YES;
-
-    //大标题
-    UILabel *label=[[UILabel alloc]init];
-    label.frame=CGRectMake(55, 45, 300, 135);
-    label.textAlignment=NSTextAlignmentCenter;
-    label.text=@"Kohakuの家";
-    label.font=[UIFont systemFontOfSize:40];
-    label.textColor=[UIColor whiteColor];
-    self.label=label;
-    
-    //小标题
-    UILabel *label2=[[UILabel alloc]init];
-     label2.frame=CGRectMake(55, 90, 300, 135);
-     label2.textAlignment=NSTextAlignmentCenter;
-     label2.text=@"换一个地方，换一种心情";
-     label2.font=[UIFont systemFontOfSize:25];
-     label2.textColor=[UIColor whiteColor];
-    self.label2=label2;
-    
-    //轮循ScrollView
-    UIScrollView *scrollView=[[UIScrollView alloc]init];
-    scrollView.frame=CGRectMake(15, 285, 384, 200);
-    self.scrollView = scrollView;
-    for(int i=0;i<5;i++)
-    {
-        UIImageView *imageView=[[UIImageView alloc]init];
-        NSString *name =[NSString stringWithFormat:@"pic%d",i+1];
-        imageView.image=[UIImage imageNamed:name];
-        imageView.frame=CGRectMake(i*384,0, 384, 200);
-        [scrollView addSubview:imageView];
-    }
-    scrollView.contentSize=CGSizeMake(5*384, 0);
- //   [self.scrollViewbg addSubview:scrollView];
-    scrollView.showsVerticalScrollIndicator = FALSE;
-    scrollView.showsHorizontalScrollIndicator = FALSE;
-    scrollView.pagingEnabled=YES;
-    
-    
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        
+    }];
+    [self initSubviews];
+    [self initConstraints];
     //页数控制
-    UIPageControl *pageControl=[[UIPageControl alloc] init];
-    pageControl.frame = CGRectMake(155, 450, 100, 20);
-    pageControl.numberOfPages=5;
-    pageControl.currentPage = 0;
-    self.pageControl.hidesForSinglePage=YES;
-    [self.scrollViewbg addSubview:pageControl];
-    self.pageControl = pageControl;
-    scrollView.delegate=self;
     [self startTimer];
-    
-    UIView *view1=[[UIView alloc]init];
-    self.view1=view1;
-    view1.frame=CGRectMake(0, 0,414,500);
-    [self.view1 addSubview:imageViewbg];
-    [self.view1 addSubview:scrollView];
-    [self.view1 addSubview:label];
-    [self.view1 addSubview:label2];
-    [self.view1 addSubview:pageControl];
-    
-    
-    
-    
-    
-    _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64,414,self.view.bounds.size.height) style:UITableViewStyleGrouped];
-    self.tableView.dataSource=self;
-    self.tableView.rowHeight=300;
-    self.tableView.tableHeaderView=view1;
+}
 
-    [self.view addSubview:_tableView];
-    [self.tableView reloadData];
+- (void)initSubviews {
+    [self.view addSubview:self.imageView];
+    [self.view addSubview:self.scrollView];
+    [self.view addSubview:self.headline];
+    [self.view addSubview:self.subhead];
+    [self.view addSubview:self.pageControl];
+    [self.view addSubview:self.tableView];
+}
 
-    CGRect screen =[[UIScreen mainScreen]bounds];
-    CGFloat navigationBarHeight=44;
-
-    UINavigationBar *navigationBar=[[UINavigationBar alloc]initWithFrame:CGRectMake(0, 20, screen.size.width, navigationBarHeight)];
-    [self.view addSubview:navigationBar];
-    UINavigationItem *navigationItem =[[UINavigationItem alloc]initWithTitle:@"琥珀博客"];
-    navigationBar.items=@[navigationItem];
+- (void)initConstraints {
+    [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top);
+        make.width.mas_equalTo(414);
+        make.height.mas_equalTo(270);
+    }];
+    [self.headline mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.imageView.mas_centerX);
+        make.centerY.equalTo(self.imageView.mas_centerY).offset(-20);
+    }];
+    [self.subhead mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.headline.mas_bottom);
+        make.centerX.equalTo(self.imageView.mas_centerX);
+    }];
+    [self.scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.imageView.mas_bottom).offset(10);
+        make.centerX.equalTo(self.imageView.mas_centerX);
+        make.size.mas_equalTo(CGSizeMake(384, 200));
+    }];
+    [self.pageControl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.scrollView.mas_bottom).offset(-20);
+        make.centerX.equalTo(self.scrollView.mas_centerX);
+        make.size.mas_equalTo(CGSizeMake(100, 20));
+    }];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.scrollView.mas_bottom).offset(20);
+        make.centerX.equalTo(self.imageView.mas_centerX);
+    }];
 }
 
 //下一页
-- (void)nextPage
-{
+- (void)nextPage{
     NSInteger page =self.pageControl.currentPage + 1;
-    if(page == 5)
-    {
+    if(page == 5){
         page=0;
     }
-    self.pageControl.currentPage=page;
+    self.pageControl.currentPage = page;
     [self.scrollView setContentOffset:CGPointMake(page * self.scrollView.frame.size.width,0) animated:YES];
 }
+
 #pragma mark -Timer 定时器
--(void)startTimer
-{
-     self.timer=[NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(nextPage) userInfo:nil repeats:YES];
+-(void)startTimer{
+    self.timer=[NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(nextPage) userInfo:nil repeats:YES];
     [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
-     
 }
 
--(void)stopTimer
-{
+-(void)stopTimer{
     [self.timer invalidate];
-    self.timer=nil;
+    self.timer = nil;
 }
 
 #pragma mark -UIcrollViewDelegate  页数控制代理
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-        int page=(int)(scrollView.contentOffset.x/scrollView.frame.size.width+0.5);
-        self.pageControl.currentPage = page;
+    int page=(int)(scrollView.contentOffset.x/scrollView.frame.size.width+0.5);
+    self.pageControl.currentPage = page;
 }
 
--(void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView
-{
+-(void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
     [self stopTimer];
 }
 
--(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
-{
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
     [self startTimer];
 }
 #pragma mark -UITableViewDataSource  表视图
-//这些方法不需要主动调用
-//告诉TableView有多少组
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     NSArray *data = [_bigDic objectForKey:@"data"];
     return data.count;
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 1;
 }
 
 //告诉tableView每一行显示的内容
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
-{
-
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
     UITableViewCell *cell =[[UITableViewCell alloc]initWithFrame:CGRectZero];
     NSArray *data = [_bigDic objectForKey:@"data"];
-    NSDictionary *Dict = data[data.count-indexPath.section-1];
+    NSDictionary *Dict = data[data.count - indexPath.section - 1];
     NSString *content = [Dict objectForKey:@"post_content"];
-    cell.textLabel.text=content;
-    cell.textLabel.numberOfLines=0;  //可多行显示
+    cell.textLabel.text = content;
+    cell.textLabel.numberOfLines = 0;  //可多行显示
     cell.textLabel.lineBreakMode=NSLineBreakByWordWrapping;//拆行
     return cell;
 }
 
 #pragma mark -代理方法
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSLog(@"TEST ++你好");
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return (tableView.rowHeight)<100?tableView.rowHeight:100;
 }
-//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath
-//{
-//    CommentViewController * commentVc = [[CommentViewController alloc]init];
-//    commentVc.topic = self.topics[indexPath.row];
-//    [self.navigationController pushViewController:commentVc animated:YES];
-//}
 
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.bounces = YES;
+        _tableView.backgroundColor = [UIColor whiteColor];
+        _tableView.rowHeight = 300;
+    }
+    return _tableView;
+}
+
+- (UIScrollView *)scrollView {
+    if (!_scrollView) {
+        _scrollView = [[UIScrollView alloc]init];
+        _scrollView.contentSize=CGSizeMake(5 * 384, 0);
+        _scrollView.showsVerticalScrollIndicator = NO;
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        _scrollView.pagingEnabled = YES;
+        _scrollView.delegate=self;
+        for(int i = 0; i < 5; i++){
+            UIImageView *imageView=[[UIImageView alloc]init];
+            NSString *name =[NSString stringWithFormat:@"pic%d",i+1];
+            imageView.image=[UIImage imageNamed:name];
+            imageView.frame=CGRectMake(i*384,0, 384, 200);
+            [_scrollView addSubview:imageView];
+        }
+    }
+    return _scrollView;
+}
+
+- (UILabel *)headline {
+    if (!_headline) {
+        _headline = [[UILabel alloc] init];
+        _headline.textAlignment=NSTextAlignmentCenter;
+        _headline.text=@"Kohakuの家";
+        _headline.font=[UIFont systemFontOfSize:40];
+        _headline.textColor=[UIColor whiteColor];
+    }
+    return _headline;
+}
+
+- (UILabel *)subhead {
+    if (!_subhead) {
+        _subhead = [[UILabel alloc] init];
+        _subhead.textAlignment=NSTextAlignmentCenter;
+        _subhead.text=@"换一个地方，换一种心情";
+        _subhead.font=[UIFont systemFontOfSize:25];
+        _subhead.textColor=[UIColor whiteColor];
+    }
+    return _subhead;
+}
+
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"背景"]];
+        _imageView.frame=CGRectMake(0 , 0, 414, 270);
+        _imageView.contentMode=UIViewContentModeScaleToFill;
+        _imageView.clipsToBounds=YES;
+    }
+    return _imageView;
+}
+
+- (UIPageControl *)pageControl {
+    if (!_pageControl) {
+        _pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 0, 100, 20)];
+        _pageControl.numberOfPages = 5;
+        _pageControl.currentPage = 0;
+        _pageControl.hidesForSinglePage = YES;
+    }
+    return _pageControl;
+}
 @end
 
 
